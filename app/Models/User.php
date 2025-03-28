f3e9858d56069e91d7490110a3e3a168a3fe0018
CyberSphinxxx <johnlemargonzales@gmail.com>
2025-03-29 03:30:52 +0800

    Add jobid field and define relationship with UserJob in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,11 +8,17 @@ use Illuminate\Database\Eloquent\Model;
     class User extends Model{
         protected $table = 'tbluser_ddsbe2';
         // column sa table
-        protected $fillable = ['username', 'password','gender'];
+        protected $fillable = ['username', 'password','gender','jobid'];
 
         //commented because i like that it has a timestamp ~cybersphinxxx
         //public $timestamps = false;
         //protected $primaryKey = 'id';
+
+        // Define relationship with UserJob
+        public function userJob()
+        {
+            return $this->belongsTo(UserJob::class, 'jobid', 'jobid');
+        }
 
         protected $hidden = ['password',];
  }
