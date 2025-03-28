c3f0eb34d00d6d45494d220439a3dee094781f7c
CyberSphinxxx <johnlemargonzales@gmail.com>
2025-03-29 03:32:34 +0800

    Add UserJob routes and refactor user routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,18 @@ $router->get('/', function () use ($router) {
 
 // Unsecured Routes
 $router->group(['prefix' => 'api'], function () use ($router) {
-    $router->get('/users', 'UserController@index'); // Get all users
-    $router->get('/users/{id}', 'UserController@show'); // Get user by ID
-    $router->post('/users', 'UserController@add'); // Create a new user
-    $router->put('/users/{id}', 'UserController@update'); // Update user
-    $router->patch('/users/{id}', 'UserController@update'); // Partial update
-    $router->delete('/users/{id}', 'UserController@delete'); // Delete user
+    $router->get('/users',['uses' => 'UserController@getUsers']);
 });
+
+// User Routes
+$router->get('/users', 'UserController@index'); // Get all users
+$router->get('/users/{id}', 'UserController@show'); // Get user by ID
+$router->post('/users', 'UserController@add'); // Create a new user
+$router->put('/users/{id}', 'UserController@update'); // Update user
+$router->patch('/users/{id}', 'UserController@update'); // Partial update
+$router->delete('/users/{id}', 'UserController@delete'); // Delete user
+
+// UserJob Routes
+$router->get('/userjobs', 'UserJobController@index'); // Get all user jobs
+$router->get('/userjobs/{id}', 'UserJobController@show'); // Get user job by ID
+
